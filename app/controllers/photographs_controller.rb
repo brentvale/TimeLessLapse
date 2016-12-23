780cc3af5e1c1d642fac8eb975b4cad16b8073b4
Brent Vale <brentvale@gmail.com>
2016-12-23 13:58:37 -0800

    uses url in production for photograph.image

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -17,7 +17,11 @@ class PhotographsController < ApplicationController
   
   def set_location
     @photograph = Photograph.find(params[:id])
-    photo = EXIFR::JPEG.new(@photograph.image.path)
+    if Rails.env.production?
+      photo = EXIFR::JPEG.new(@photograph.image.path)
+    else
+      photo = EXIFR::JPEG.new(@photograph.image.url)
+    end
     #"37.4215***-121.7606" back door coordinates for garden timelapse 
     @photograph_coords = photo.gps.nil? ? "37.4215***-121.7606" : photo.gps.latitude.to_s.concat("***").concat(photo.gps.longitude.to_s) 
     @hubs = current_user.timelapse_hubs.map{|x| "#{x.hub_name}***#{x.latitude}***#{x.longitude}***#{x.id}"}
